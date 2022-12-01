0af322ff063c926870e60cee4a743b941446b220
Artyom Afanasov <afanasov.artyom@gmail.com>
2022-12-01 21:08:14 +0400

    Revert "Revert "Revert "Revert "Improving the API (without token usage) and memory store""""

---
+++ Services/IProfileService.cs
@@ -0,0 +1,14 @@
+﻿using CViewer.DataAccess.Entities;
+
+namespace CViewer.Services
+{
+    public interface IProfileService
+    {
+        public Profile SignUp(UserCredentials userCredentials);
+        public Profile SignIn(UserCredentials userCredentials);
+        public Profile GetProfile(int profileId);
+        public Profile UpdateProfile(int profileId, string firstName = null, string lastName = null, string biography = null,
+            double? rating = null, string email = null, string password = null, int? specializationId = null);
+        public List<Profile> ListProfiles();
+    }
+}
