a39c628fdd38f2ab46f3de38ad589397f5d048c9
ArtyomAfanasovBS <afanasov.artyom.bs@gmail.com>
2022-12-15 14:54:38 +0400

    Fixed log

--- DataAccess/TransitObjects/ComplexCVAndIFormFile.cs
+++ DataAccess/TransitObjects/ComplexCVAndIFormFile.cs
@@ -29,9 +29,29 @@ namespace CViewer.DataAccess.TransitObjects
             logger.Write(LogEventLevel.Information, $"BindAsync {nameof(ComplexCVAndIFormFile)}: {nameof(cvDraft)} is null?: {cvDraft == null}\n");
 
             logger.Write(LogEventLevel.Information,
-                $"BindAsync {nameof(ComplexCVAndIFormFile)}: {nameof(cvDraft.Title)}: {cvDraft.Title}\n{nameof(cvDraft.FileName)}: {cvDraft.FileName}\nCount of {nameof(cvDraft.Tags)}: {cvDraft.Tags.Count}\n");
+                $"BindAsync {nameof(ComplexCVAndIFormFile)}: \n{nameof(cvDraft.FileName)}: {cvDraft.FileName}");
+
+            logger.Write(LogEventLevel.Information,
+                cvDraft.Tags != null
+                    ? $"BindAsync {nameof(ComplexCVAndIFormFile)}: Count of {nameof(cvDraft.Tags)}: {cvDraft.Tags.Count}\n"
+                    : $"BindAsync {nameof(ComplexCVAndIFormFile)}: {nameof(cvDraft.Tags)} is null\n");
+
+            logger.Write(LogEventLevel.Information,
+                cvDraft.Title != null
+                    ? $"BindAsync {nameof(ComplexCVAndIFormFile)}: {nameof(cvDraft.Title)}: {cvDraft.Title}\n"
+                    : $"BindAsync {nameof(ComplexCVAndIFormFile)}: {nameof(cvDraft.Title)} is null\n");
+
+            logger.Write(LogEventLevel.Information,
+                cvDraft.FileName != null
+                    ? $"BindAsync {nameof(ComplexCVAndIFormFile)}: {nameof(cvDraft.FileName)}: {cvDraft.FileName}\n"
+                    : $"BindAsync {nameof(ComplexCVAndIFormFile)}: {nameof(cvDraft.FileName)} is null\n");
 
             IFormFile file = form.Files[nameof(File)];
+
+            if (file == null)
+            {
+                logger.Write(LogEventLevel.Information, $"BindAsync {nameof(ComplexCVAndIFormFile)}: file is null\n");
+            }
 
             if (file != null)
             {
