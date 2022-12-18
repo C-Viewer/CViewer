0fd2333eb5165c226978ef915df438ec0cd129a5
Polina Musatova <99819809+Polina1111111112@users.noreply.github.com>
2022-12-18 03:59:17 +0300

    Update CVTagRepository.cs

--- DataAccess/Repositories/CVTagRepository.cs
+++ DataAccess/Repositories/CVTagRepository.cs
@@ -31,6 +31,56 @@ namespace CViewer.DataAccess.Repositories
                 Id = 5,
                 Name = "Python",
             },
+            new CVTag()
+            {
+                Id = 6,
+                Name = "Figma",
+            },
+            new CVTag()
+            {
+                Id = 7,
+                Name = "C++",
+            },
+            new CVTag()
+            {
+                Id = 8,
+                Name = "Android Studio",
+            },
+            new CVTag()
+            {
+                Id = 9,
+                Name = "Jira Atlassian",
+            },
+            new CVTag()
+            {
+                Id = 10,
+                Name = "Git",
+            },
+            new CVTag()
+            {
+                Id = 11,
+                Name = "Docker",
+            },
+            new CVTag()
+            {
+                Id = 12,
+                Name = "Linux",
+            },
+            new CVTag()
+            {
+                Id = 13,
+                Name = "Swagger UI",
+            },
+            new CVTag()
+            {
+                Id = 14,
+                Name = "Postman",
+            },
+            new CVTag()
+            {
+                Id = 15,
+                Name = "Angular",
+            },
         };
 
         public static HashSet<int> CVTagIds = CVTags.Select(tag => tag.Id).ToHashSet();
